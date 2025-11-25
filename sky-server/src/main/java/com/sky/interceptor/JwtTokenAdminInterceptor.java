85d92b9439c12c60ccc4c931ff63b40f8151e6d4
heyigongmian <2248093666@qq.com>
2025-11-25 10:21:27 +0800

    苍穹外卖项目初试代码

---
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenAdminInterceptor.java
@@ -0,0 +1,57 @@
+package com.sky.interceptor;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.properties.JwtProperties;
+import com.sky.utils.JwtUtil;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * jwt令牌校验的拦截器
+ */
+@Component
+@Slf4j
+public class JwtTokenAdminInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 校验jwt
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.判断当前拦截到的是Controller的方法还是其他资源
+        if(handler instanceof HandlerMethod){
+            //当前拦截到的是Controller的方法
+            return true;
+        }
+        //2.获取请求头中的令牌
+        String token = request.getHeader(jwtProperties.getAdminTokenName());
+
+        //3.校验令牌
+        try{
+            log.info("开始校验令牌：{}", token);
+            Claims claims = JwtUtil.parseJWT(jwtProperties.getAdminSecretKey(), token);
+            Long empId = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
+            log.info("令牌校验通过，用户id：{}", empId);
+            //4.放行
+            return true;
+        } catch (Exception ex){
+            //5.校验失败，拦截
+            response.setStatus(401);
+            return false;
+        }
+    }
+}
